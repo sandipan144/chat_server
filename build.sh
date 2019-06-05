f0383a6e0d028cc1087bef8eaefd3c9fa0de43ed
Sandipan Chakraborty <sandipanchakraborty144@gmail.com>
2019-06-06 02:40:34 +0530

    .

--- build.sh
+++ build.sh
@@ -1,6 +1,9 @@
 echo "Compiling Server Application"
 
+mkdir -p bin
 gcc -o bin/server server/main/src/server_main.c
 
 echo 
 echo "Code Compiled Successfully"
+echo
+echo "Binary files created in bin folder"
