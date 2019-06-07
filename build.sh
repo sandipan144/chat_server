f0e8b053015f5107c43018d33e724e745d6adc67
Sandipan Chakraborty <sandipanchakraborty144@gmail.com>
2019-06-07 23:38:59 +0530

    server and client code added

--- build.sh
+++ build.sh
@@ -1,7 +1,13 @@
 echo "Compiling Server Application"
 
+set -e
 mkdir -p bin
 gcc -o bin/server server/main/src/server_main.c
+
+echo
+echo "Compiling Client Application"
+
+gcc -o bin/client client/main/src/client_main.c
 
 echo 
 echo "Code Compiled Successfully"
--- server/main/src/server_main.c
+++ server/main/src/server_main.c
@@ -14,9 +14,69 @@ int main(int argc, char *argv[])
 {
 	if(argc < 2)
 	{
-		printf("Port Number not provided. Program terminated.\n");
+		fprintf(stderr, "Port Number not provided. Program terminated.\n");
 		exit(1);
 	}
 
+	int sockfd, newsockfd, portno, n;
+	char buffer[255];
 
+	struct sockaddr_in serv_addr, cli_addr;
+	socklen_t clilen;
+
+	sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	if(sockfd < 0)
+	{
+		error("Error opening Socket");
+	}
+
+	bzero((char *) &serv_addr, sizeof(serv_addr));
+	portno = atoi(argv[1]);
+
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_addr.s_addr = INADDR_ANY;
+	serv_addr.sin_port = htons(portno);
+
+	if(bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0)
+	{
+		error("Binding failed");
+	}
+
+	listen(sockfd, 5);
+	clilen = sizeof(cli_addr);
+	newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
+	if(newsockfd < 0)
+	{
+		error("Error in Accept");
+	}
+
+	while(1)
+	{
+		bzero(buffer, 255);
+		n = read(newsockfd, buffer, 255);
+		if(n < 0)
+		{
+			error("Error in reading");
+		}
+		printf("Client(recieve): %s\n", buffer);
+		
+		bzero(buffer, 255);
+		printf("Server(send):");
+		fgets(buffer, 255, stdin);
+		n = write(newsockfd, buffer, strlen(buffer));
+		if(n < 0)
+		{
+			error("Error on writting");
+		}
+
+		int i = strcmp("Bye", buffer);
+		if(i == 0)
+		{
+			break;
+		}
+	}
+
+	close(newsockfd);
+	close(sockfd);
+	return 0;
 }
